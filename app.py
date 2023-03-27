ccc1e01b4f5bc5ac52d9e0aa8ab593d0bf8bdcc6
Noah Piraino <noahapiraino@gmail.com>
2023-03-27 12:31:34 -0400

    commit hooks for styling

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
-@app.get('/')
+
+@app.get("/")
 def index():
-    return render_template('index.html', home_active=True)
+    return render_template("index.html", home_active=True)
--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -2,6 +2,6 @@ import pytest
 from app import app
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope="module")
 def test_app():
     return app.test_client()
