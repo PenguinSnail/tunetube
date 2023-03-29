bb07e6a3a15348c3da1b7bcbf81183c5ee88a021
joe sunshine <joesun02@gmail.com>
2023-03-29 13:17:10 -0400

    creating tune_library commit

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 @app.get("/")
 def index():
     return render_template("index.html", home_active=True)
+
+
+@app.get("/library")
+def library():
+    return render_template("tune_library.html", home_active=True)
