9d9148527c10e7847d116e7f94c93adda93d4efc
Ethan Hurley <ehurley4@uncc.edu>
2023-03-28 21:16:23 -0400

    landing page

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ app = Flask(__name__)
 @app.get("/")
 def index():
     return render_template("index.html", home_active=True)
+
+
+@app.get("/landing_page")
+def landing_page():
+    return render_template("landing_page.html")
