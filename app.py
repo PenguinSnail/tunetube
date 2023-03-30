a2d8ccf6eeded4d18ea4d5cd615abaf0da43bfbf
frankyToast <94389782+frankyToast@users.noreply.github.com>
2023-03-29 20:49:34 -0400

    Changed app route for home page

--- app.py
+++ app.py
@@ -7,8 +7,3 @@ app = Flask(__name__)
 @app.get("/")
 def index():
     return render_template("index.html", home_active=True)
-
-
-@app.get("/home")
-def home_page():
-    return render_template("home-page.html", home_active=True)
