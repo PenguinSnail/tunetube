d5f46884905097018207bf60712a5be9acba9ce4
tylerJordan223 <tjlinkmaster22@gmail.com>
2023-04-12 19:31:35 -0400

    Implemented User Model

--- src/models.py
+++ src/models.py
@@ -8,3 +8,10 @@ class Posts(db.Model):
     post_name = db.Column(db.String(255), nullable=False)
     song = db.Column(db.JSON, nullable=False)
     creator = db.Column(db.Integer, nullable=False)
+
+
+class Users(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), nullable=False)
+    password = db.Column(db.String(15), nullable=False)
+    photoID = db.Column(db.Integer, db.ForeignKey("photoID"))
