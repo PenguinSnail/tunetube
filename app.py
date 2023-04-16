4951d2558a408359045de08cf903c03e8e84ff52
frankyToast <94389782+frankyToast@users.noreply.github.com>
2023-04-16 00:22:36 -0400

    Added to the app.py to fulfill the needs for the post and home page

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
+from src.repositories.post_repository import post_repository_singleton
 
 
 app = Flask(__name__)
 
 
-@app.get("/")
-def index():
-    return render_template("index.html", home_active=True)
+# get's usuer ID to create feed
+@app.get("/user_ID")
+def index(user_ID):
+    feed = post_repository_singleton.get_user_feed(user_ID)
+    return render_template("index.html", home_active=True, feed=feed)
+
+
+# gets a specific post from the feed
+@app.get("/post/<int:post_id>")
+def get_single_post(post_id):
+    single_post = post_repository_singleton.get_post_by_id(post_id)
+    return render_template("post.html", home_active=True, post=single_post)
---
+++ src/repositories/post_repository.py
@@ -0,0 +1,29 @@
+# PlaceHolder
+
+# from src.models import Movie, db
+
+
+# class MovieRepository:
+
+#     def get_all_movies(self):
+#         movies = Movie.query.all()
+#         # print(movies)
+#         return movies
+
+#     def get_movie_by_id(self, ID):
+#         # TODO get a single movie from the DB using the ID
+#         movie = Movie.query.filter_by(movie_id = ID).first()
+#         return movie
+
+#     def create_movie(self, title, director, rating):
+#         new_movie = Movie(title,director,rating)
+#         db.session.add(new_movie)
+#         db.session.commit()
+#         return new_movie
+
+#     def search_movies(self, t):
+#         movie = Movie.query.filter(Movie.title.contains(t)).all()
+#         return movie
+
+# # Singleton to be used in other modules
+# movie_repository_singleton = MovieRepository()
