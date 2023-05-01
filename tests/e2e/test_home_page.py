6aa7919c2ed102d70acd8fa639b6833d29086414
Noah Piraino <noahapiraino@gmail.com>
2023-04-30 20:59:39 -0400

    test for landing page buttons

--- tests/e2e/test_home_page.py
+++
@@ -1,8 +0,0 @@
-# from flask.testing import FlaskClient
-#
-#
-# def test_home_page(test_app: FlaskClient):
-#     response = test_app.get("/")
-#     response_data = response.data.decode("utf-8")
-#
-#     assert "<h1>TuneTube</h1>" in response_data
