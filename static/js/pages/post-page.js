66d19b4e677373adc627a5352de4e8ba5ed4444b
Noah Piraino <noahapiraino@gmail.com>
2023-05-11 11:30:51 -0400

    small spacing tweaks, fix post page playback

--- static/js/pages/post-page.js
+++ static/js/pages/post-page.js
@@ -4,4 +4,4 @@ import { setupCard } from "../post-card.js";
 const context = new AudioContext();
 const player = new Player(context);
 
-setupCard(document.querySelector("button.play-button"), player, false);
+setupCard(document.querySelector(".post-card"), player, false);
