9491765de3cbd245008a95a3fea48829d7acaf76
Ethan Hurley <ehurley4@uncc.edu>
2023-05-11 20:28:55 -0400

    post testing

--- tests/e2e/test_post_pages.py
+++ tests/e2e/test_post_pages.py
@@ -3,24 +3,36 @@ from flask.testing import FlaskClient
 from src.models import db, User, Post
 from flask_bcrypt import Bcrypt
 from src.repositories.user_repository import user_repository_singleton
-from tests.utils import refresh_db
+from tests.utils import refresh_db, create_user, create_post
 from app import session
 
 def test_post_pages(test_client: FlaskClient):
     refresh_db()
-    response = test_client.post('/account/register', query_string={
-        'name': 'user', 'password': '1234', 'confirm_password': '1234' 
-    }, follow_redirects=True)
+
+    with test_client.session_transaction() as session:
+        test_user = create_user()
+
+        session['user'] = {
+            'user_id' : test_user.getID
+        }
+
+    test_post = create_post(post=session['user_id'])
+
+    # Now you should be able to go into the test files. 
+
+    # response = test_client.post('/account/register', query_string={
+    #     'name': 'user', 'password': '1234', 'confirm_password': '1234' 
+    # }, follow_redirects=True)
 
 
-    test_client.post('/tunes', query_string={
-        'title':'My Song', 
-        'data':'[{"time":702,"frequency":261.63,"playing":true},{"time":795,"frequency":261.63,"playing":false},{"time":1018,"frequency":261.63,"playing":true},{"time":1137,"frequency":261.63,"playing":false}]'
-        }, follow_redirects=True, cookies={'session': session})
+    # test_client.post('/tunes', query_string={
+    #     'title':'My Song', 
+    #     'data':'[{"time":702,"frequency":261.63,"playing":true},{"time":795,"frequency":261.63,"playing":false},{"time":1018,"frequency":261.63,"playing":true},{"time":1137,"frequency":261.63,"playing":false}]'
+    #     }, follow_redirects=True, cookies={'session': session})
     
 
 
-    response = test_client.get('/tunes', cookies={'session': session})
-    response_data = response.data.decode('utf-8')
-    assert 'My Song' in response_data
+    # response = test_client.get('/tunes', cookies={'session': session})
+    # response_data = response.data.decode('utf-8')
+    # assert 'My Song' in response_data
 
--- tests/utils.py
+++ tests/utils.py
@@ -9,3 +9,13 @@ def refresh_db():
     Photo.query.delete()
     User.query.delete()
     db.session.commit()
+
+def create_user(username='tester', password='1234'):
+    testing_user = User(username=username, password=password)
+    db.session.commit
+    return testing_user
+
+def create_post(title='tempSong', song='{"time":702,"frequency":261.63,"playing":true}'):
+    post = Post(title=title, song=song)
+    db.session.add(post)
+    db.session.commit()
